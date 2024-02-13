6482a2ed8fa18f7a8fe71ccfc4d4b7f5a2482a96
7URBO <159884612+sevenURBO@users.noreply.github.com>
2024-02-13 12:16:21 +0100

    Update 2d_grid_creator.py

--- 2d_grid_creator.py
+++ 2d_grid_creator.py
@@ -1,8 +1,9 @@
+#creating grid and filling with zeros
 def create_grid(rows, cols):
     grid = [[0 for _ in range(cols)] for _ in range(rows)]
     return grid
 
-
+#printing grid in different rows
 def print_grid(grid):
     for row in grid:
         print(row)
